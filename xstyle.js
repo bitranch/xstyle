fff9883ba5e175dfea26a3963d12332538598edb
bitranch <bill@bitranch.com>
2012-11-25 23:12:56 -0500

    sometimes there is no sheet

--- xstyle.js
+++ xstyle.js
@@ -29,6 +29,9 @@ define("xstyle/xstyle", ["require"], function (require) {
 		ua.indexOf("Opera") > -1 ? "-o-" : "";
 	function checkImports(element, callback, fixedImports){
 		var sheet = element.sheet || element.styleSheet;
+        if (!sheet) {
+            return;
+        }
 		var needsParsing = sheet.needsParsing, // load-imports can check for the need to parse when it does its recursive look at imports 
 			cssRules = sheet.rules || sheet.cssRules;
 		function fixImports(){
